3faf28af3d3350c2d60527c1a49add78f6e6b086
Daviti Vashakmadze <vashaymadzedavit@gmail.comgit config --global user.name Datuchaaa>
2023-08-08 20:15:17 +0400

    feat: add Formik's form

--- src/pages/QRCodeGenerator.tsx
+++ src/pages/QRCodeGenerator.tsx
@@ -1,35 +1,52 @@
-import React, { useState } from 'react';
-import QRCode from 'qrcode';
-import ExportQRImage from '../components/ExportQRImage';
-import './QRCodeGenerator.scss'
+import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import QRCode from "qrcode";
+import ExportQRImage from "../components/ExportQRImage";
+import "./QRCodeGenerator.scss";
 
 const QRCodeGenerator: React.FC = () => {
-  const [text, setText] = useState('');
   const [qrCodeData, setQRCodeData] = useState<string | null>(null);
 
-  const generateQRCode = async () => {
+  const generateQRCode = async (text: string) => {
     try {
       const qrCodeData = await QRCode.toDataURL(text);
-      setQRCodeData(qrCodeData);
+      return qrCodeData;
     } catch (error) {
-      
+      return null;
     }
   };
 
   return (
     <div>
-      <h1>QR Code Generator</h1>
-      <div>
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="Enter text here"
-        />
-        <button onClick={generateQRCode}>Generate QR Code</button>
+      <div className="container">
+        <h1>QR Code Generator</h1>
+        <Formik
+          initialValues={{ text: "" }}
+          validationSchema={Yup.object({
+            text: Yup.string().required("Text is required"),
+          })}
+          onSubmit={async (values, { setSubmitting }) => {
+            const qrCodeData = await generateQRCode(values.text);
+            setQRCodeData(qrCodeData);
+            setSubmitting(false);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <div>
+                <Field type="text" name="text" placeholder="Enter text here" />
+                <ErrorMessage name="text" component="div" className="error" />
+              </div>
+              <button type="submit" disabled={isSubmitting}>
+                Generate QR Code
+              </button>
+            </Form>
+          )}
+        </Formik>
+        {qrCodeData && <img src={qrCodeData} alt="QR Code" />}
+        {qrCodeData && <ExportQRImage qrCodeData={qrCodeData} />}
       </div>
-      {qrCodeData && <img src={qrCodeData} alt="QR Code" />}
-      {qrCodeData && <ExportQRImage qrCodeData={qrCodeData} />}
     </div>
   );
 };
