66b5ddf11aa3cd6caffdbfb06b3acf9e7b61a124
Daviti Vashakmadze <vashaymadzedavit@gmail.comgit config --global user.name Datuchaaa>
2023-08-08 17:19:40 +0400

    init: create qr generator

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import QRCode from 'qrcode';
+
+const Home: React.FC = () => {
+  const [text, setText] = useState('');
+  const [qrCodeData, setQRCodeData] = useState<string | null>(null);
+
+  const generateQRCode = async () => {
+    try {
+      const qrCodeData = await QRCode.toDataURL(text);
+      setQRCodeData(qrCodeData);
+    } catch (error) {
+      console.error('Error generating QR code:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>QR Code Generator</h1>
+      <div>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Enter text here"
+        />
+        <button onClick={generateQRCode}>Generate QR Code</button>
+      </div>
+      {qrCodeData && <img src={qrCodeData} alt="QR Code" />}
+    </div>
+  );
+};
+
+export default Home;
